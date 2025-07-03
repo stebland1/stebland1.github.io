49e466a6843c66dfdf373f908d528d1a735eb4e3
Stephen Bland <stebland1@gmail.com>
2025-07-03 21:40:46 +0100

    utils: create hyperscript helper

---
+++ assets/js/utils/hyperscript.js
@@ -0,0 +1,27 @@
+/**
+ * @template {keyof HTMLElementTagNameMap} K
+ * @param {K} tagName
+ * @param {Partial<HTMLElementTagNameMap[K]> & {[key: string]: any}} [props]
+ * @param {Array<(Node | string)>} children
+ *
+ * @returns {HTMLElementTagNameMap[K]}
+ */
+export const h = (tagName, props = {}, ...children) => {
+  const el = document.createElement(tagName);
+
+  for (const [key, value] of Object.entries(props)) {
+    if (key in el) {
+      el[key] = value;
+    } else {
+      el.setAttribute(key, value);
+    }
+  }
+
+  for (const child of children) {
+    el.appendChild(
+      child instanceof Node ? child : document.createTextNode(child),
+    );
+  }
+
+  return el;
+};
